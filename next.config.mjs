4760d57422c3d8effae43830abe5cecf91c50b41
Dody <dodyags@gmail.com>
2024-03-07 23:38:23 +0700

    add docker and github action

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,15 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
+
+// https://github.com/vercel/next.js/tree/canary/examples/with-docker
+module.exports = {
+  output: "standalone",
+};
