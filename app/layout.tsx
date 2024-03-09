345ac2df17537c97d30be919aed4a7c87e55353a
Dody <dodyags@gmail.com>
2024-03-09 12:34:05 +0700

    disable next analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "../global.css";
 import { Inter } from "next/font/google";
 import LocalFont from "next/font/local";
 import { Metadata } from "next";
-import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://chronark.com"),
@@ -62,16 +61,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
-      {/* <head>
-        <Analytics />
-      </head> */}
       <body
         className={`bg-black ${
           process.env.NODE_ENV === "development" ? "debug-screens" : undefined
         }`}
       >
         {children}
-        <Analytics />
       </body>
     </html>
   );
