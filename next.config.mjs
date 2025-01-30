37c263e641aef6221cab0892a7d877268be1da07
Dody <dodyags@gmail.com>
2025-01-30 14:54:13 +0700

    fix: [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 import { withContentlayer } from "next-contentlayer2";
 
+// https://stackoverflow.com/questions/78516645/warning-deprecationwarning-the-punycode-module-is-deprecated-please-use-a-u
+// https://github.com/mathiasbynens/punycode.js/issues/137
+import moduleAlias from "module-alias";
+moduleAlias.addAlias("punycode", "punycode/");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
